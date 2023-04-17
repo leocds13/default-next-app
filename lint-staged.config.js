18aabc6a92bb95747df0ce448aeed9d50ff67cfb
Leonardo Cabral <leocab.san@gmail.com>
2023-04-17 00:19:28 -0300

    init

---
+++ lint-staged.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Type check TypeScript files
+  "./src/**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+
+  // Lint then format TypeScript and JavaScript files
+  "./src/**/*.(ts|tsx|js)": filenames => [
+    `yarn eslint --fix ${filenames.join(" ")}`,
+    `yarn prettier --write ${filenames.join(" ")}`,
+  ],
+
+  // Format MarkDown and JSON
+  "**/*.(md|json)": filenames => `yarn prettier --write ${filenames.join(" ")}`,
+};
